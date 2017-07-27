5f34888fc731257e651539a3f8c7eb3471cc638a
Ignat Remizov <ignat980@gmail.com>
2017-07-27 16:45:09 -0400

    Using constants

--- index.php
+++ index.php
@@ -6,24 +6,20 @@
     </head>
     <body>
       <?php
-        // null == NULL aka case insensitive
-        $var1 = null;
-        $var2 = "";
+        $max_width = 980; //variable
+
+        define("MAX_WIDTH", 980); //constant
+        echo MAX_WIDTH;
       ?>
-
-      Is var1 null? <?= is_null($var1)?><br>
-      Is var2 null? <?= is_null($var2)?><br>
-      Is var3 null? <?= is_null($var3)?><br>
       <br>
-      Is var1 set? <?= isset($var1)?><br>
-      Is var2 set? <?= isset($var2)?><br>
-      Is var3 set? <?= isset($var3)?><br>
-      <br>
+      <?php
+        //can't change value
+        // MAX_WIDTH = MAX_WIDTH + 1;
+        // echo MAX_WIDTH;
 
-      <?php // empty: "", null, 0, 0.0, "0", false, array()?>
-      <?php $var3 = "0";?>
-      Is var1 empty? <?= empty($var1)?><br>
-      Is var2 empty? <?= empty($var2)?><br>
-      Is var3 empty? <?= empty($var3)?><br>
+        //can't redefine it either
+        // define("MAX_WIDTH", 981);
+        // echo MAX_WIDTH;
+      ?>
     </body>
 </html>
