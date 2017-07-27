2e849fad01231ed30f9762157f0aceac85cea4b3
Ignat Remizov <ignat980@gmail.com>
2017-07-27 15:19:56 -0400

    Using arrays

--- index.php
+++ index.php
@@ -1,32 +1,38 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>I'm learning to PHP</title>
     </head>
     <body>
-      <?= $float = 3.14 ?><br/>
-      <?= $float + 7 ?><br/>
-      <?= 4/3 ?><br/>
-      <br/>
-      Round: <?= round($float, 1) ?><br/>
-      Ceiling: <?= ceil($float) ?><br/>
-      Floor: <?= floor($float) ?><br/>
-      <br>
-      <?= $integer = 3 ?>
-      <?= "Is {$integer} an integer? " . is_int($integer) ?><br/>
-      <?= "Is {$float} an integer? " . is_int($float) ?><br/>
-      <br>
-      <?= "Is {$integer} a float? " . is_float($integer) ?><br/>
-      <?= "Is {$float} a float? " . is_float($float) ?><br/>
-      <br>
-      <?= "Is {$integer} numeric? " . is_numeric($integer) ?><br/>
-      <?= "Is {$float} numeric? " . is_numeric($float) ?><br/>
-      <br>
+      <?php
+        $numbers = array(4,8,15,16,23,42);
+        echo $numbers[0] . "<br>";
+        echo var_dump($numbers) . "<br>";
+
+        $mixed = array(6, 'fox', 'dog', array('x','y','z'));
+        echo $mixed[2] . "<br>";
+        echo $mixed[3] . "<br>";
+        echo $mixed . "<br>";
+        echo "<pre>";
+        echo var_dump($mixed) . "<br>";
+      ?>
+        <?php echo print_r($mixed) . "<br>";?>
+      </pre>
+      <?php
+        echo $mixed[3][0];
+
+        $mixed[2] = "cat";
+        $mixed[4] = "mouse";
+        $mixed[] = "house";
+      ?>
+      <pre>
+        <?php echo print_r($mixed) . "<br>";?>
+      </pre>
+
+      <?php
+        //PHP 5.4 or greater
+        $array = [1,2,3];
+      ?>
     </body>
 </html>
