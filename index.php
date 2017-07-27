e62a8cec3ca5d7fda72ea30ed2fcf8f41fec4131
Ignat Remizov <ignat980@gmail.com>
2017-07-27 15:36:27 -0400

    Using dictionaries (or in php, associative arrays)

--- index.php
+++ index.php
@@ -6,33 +6,19 @@
     </head>
     <body>
       <?php
+        $assoc = array('first_name' => 'Kevin', 'last_name' => 'Smith');
+      ?>
+      <?= $assoc['first_name'];?><br>
+      <?= $assoc['first_name'] . " " . $assoc['last_name'];?><br>
+
+      <?php $assoc['first_name'] = 'Larry';?>
+      <?= $assoc['first_name'] . " " . $assoc['last_name'];?><br>
+
+      <?php
         $numbers = array(4,8,15,16,23,42);
-        echo $numbers[0] . "<br>";
-        echo var_dump($numbers) . "<br>";
-
-        $mixed = array(6, 'fox', 'dog', array('x','y','z'));
-        echo $mixed[2] . "<br>";
-        echo $mixed[3] . "<br>";
-        echo $mixed . "<br>";
-        echo "<pre>";
-        echo var_dump($mixed) . "<br>";
-      ?>
-        <?php echo print_r($mixed) . "<br>";?>
-      </pre>
-      <?php
-        echo $mixed[3][0];
-
-        $mixed[2] = "cat";
-        $mixed[4] = "mouse";
-        $mixed[] = "house";
-      ?>
-      <pre>
-        <?php echo print_r($mixed) . "<br>";?>
-      </pre>
-
-      <?php
-        //PHP 5.4 or greater
-        $array = [1,2,3];
+        // ==
+        $numbers = array(0 => 4, 1 => 8, 2 => 15, 3 => 16, 4 => 23, 5 => 42);
+        echo $numbers[0];
       ?>
     </body>
 </html>
