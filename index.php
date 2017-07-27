f1486dd37903ed635efcf81df1d724675729bd9d
Ignat Remizov <ignat980@gmail.com>
2017-07-27 13:01:10 -0400

    echo statements

--- index.php
+++ index.php
@@ -4,14 +4,14 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <title>I'm learning to PHP</title>
     </head>
     <body>
-        <?php
-        phpinfo();
-        ?>
+        <?php echo "Hello world!"; ?><br />
+        <?php echo "Hello" . " world!";?><br />
+        <?php echo "2+3="; echo 2+3; ?>
     </body>
 </html>
