3e237ebce00114650b01d5db89f57ff077857ee7
Ignat Remizov <ignat980@gmail.com>
2017-07-28 10:33:25 -0400

    Using arguments in functions

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>I'm learning to PHP</title>
-    </head>
-    <body>
-      <?php
-        function say_hello() {
-          echo "Hello World! <br>";
-        }
+  <head>
+    <meta charset="UTF-8">
+    <title>I'm learning to PHP</title>
+  </head>
+  <body>
+    <?php
+      function say_hello() {
+        echo "Hello World! <br>";
+      }
 
-        say_hello();
+      say_hello();
 
-        function say_hello_to($word) {
-          echo "Hello {$word}! <br>";
-        }
+      function say_hello_to($word) {
+        echo "Hello {$word}! <br>";
+      }
 
-        say_hello_to("Me");
-        say_hello_to("Everyone");
+      say_hello_to("Me");
+      say_hello_to("Everyone");
 
-        say_hello_loudly();
-        
-        //function definitions get hoisted
-        function say_hello_loudly() {
-          echo "HELLO WOOOOORLD!!<br>";
-        }
-      ?>
-    </body>
+      say_hello_loudly();
+
+      //function definitions get hoisted
+      function say_hello_loudly() {
+        echo "HELLO WOOOOORLD!!<br>";
+      }
+
+      function paint($room='office',$color='red') {
+        return "The color of the {$room} is {$color}.<br>";
+      }
+
+      echo paint();
+      echo paint('bedroom','blue');
+      echo paint('bedroom', null);
+      echo paint('bedroom');
+      echo paint('blue');
+    ?>
+  </body>
 </html>
