d7ec398ec2bbfa9d049cd479a4739cd2e6024e3e
Ignat Remizov <ignat980@gmail.com>
2017-07-29 10:58:49 -0400

    Making a web form

--- index.php
+++ index.php
@@ -1,28 +1,15 @@
-<?php
-//!!!! IMPORTANT No characters before php for redirects, unless you have output buffering turned on.
-  function redirect_to($new_location) {
-    // This is how to make a 302 redirect
-    // Usually you would have to add the header "HTTP 1.1/ 302 Found"
-    // But php adds it automatically when you do "Location: ..."
-    header("Location: {$new_location}");
-    exit;
-  }
-
-  $logged_in = $_GET['logged_in'];
-  if ($logged_in == 1) {
-    redirect_to('redirect.php');
-  } else {
-    redirect_to("http://www.google.com");
-  }
-
-?>
-
 <!DOCTYPE html>
 <html lang='en'>
   <head>
     <meta charset='UTF-8'>
-    <title>Redirect</title>
+    <title>Form</title>
   </head>
   <body>
+    <form action="form_processing.php" method="post">
+      Username: <input type="text" name="username" value=""> <br>
+      Password  <input type="password" name="password" value=""><br>
+      <br>
+      <input type="submit" name="submit" value="Submit">
+    </form>
   </body>
 </html>
