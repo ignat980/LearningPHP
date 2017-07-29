a554bb511b8b39323336e0d575816e7d657a0611
Ignat Remizov <ignat980@gmail.com>
2017-07-29 09:46:16 -0400

    Modyfying HTML Headers

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <?php
-  include_once 'included_functions.php';
-  include 'included_header.php';
-?>
-    The header has been included.
-    <br>
-    <?= hello('Everyone')?><br>
+  header("HTTP 1.1/ 404 Not Found");
+  header("X-Powered-By: none of your beeswax");
+?><!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='UTF-8'>
+    <title>Headers</title>
+  </head>
+  <body>
+    <?php
+      // This won't work unless you have output buffering turned on.
+      // header("HTTP 1.1/ 404 Not Found")
+    ?>
+
+    <pre>
+      <?php print_r(headers_list())?>
+    </pre>
   </body>
 </html>
