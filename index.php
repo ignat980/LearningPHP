4349e99a3544110ee60c114d91d4d0f91fee530e
Ignat Remizov <ignat980@gmail.com>
2017-07-29 13:18:00 -0400

    Making a single login page

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+  require_once 'included_functions.php';
+
+  if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if ($username == "bob" && $password == "pass") {
+      //Success !
+      redirect_to('basic.html');
+    } else {
+      $message = 'There were some errors.';
+    }
+  } else {
+    $message = 'Please log in.';
+    $username = '';
+  }
+?>
 <!DOCTYPE html>
 <html lang='en'>
   <head>
@@ -5,8 +22,9 @@
     <title>Form</title>
   </head>
   <body>
-    <form action="form_processing.php" method="post">
-      Username: <input type="text" name="username" value=""> <br>
+     <?= $message ?> <br>
+    <form action="index.php" method="post">
+      Username: <input type="text" name="username" value="<?= htmlspecialchars($username) ?>"> <br>
       Password  <input type="password" name="password" value=""><br>
       <br>
       <input type="submit" name="submit" value="Submit">
